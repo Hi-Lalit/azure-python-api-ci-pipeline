8e2a8fd60b7433644df02528534bc0add55370e1
Lalit Kumar Gautam <hi.lkgautam@gmail.com>
2026-07-16 22:29:16 +0530

    updated

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return jsonify({
-        "message": "Hello World!",
-        "status": "Success"
-    })
-
-@app.route("/health")
-def health():
-    return jsonify({
-        "status": "Healthy"
-    })
+def hello_world():
+    return "Hello, World! Lalit"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
